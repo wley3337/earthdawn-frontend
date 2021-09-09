9216272a8fe5230c10d37ec4b2802a0926dd07e8
Will Ley <wley3337@gmail.com>
2021-09-09 10:32:31 -0400

    WIP for moving to other laptop

--- src/redux/Login/actions.ts
+++ src/redux/Login/actions.ts
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { takeEvery, call } from "redux-saga/effects";
+
 import { History } from "history";
 import { EARTHDAWN_URL } from "../../util/baseUrls";
 
@@ -32,6 +34,20 @@ export function* watchSubmitLogin() {
 }
 
 export function* handleSubmitLogin(action: SubmitLoginFormAction) {
-  console.log("here", { EARTHDAWN_URL });
-  const options = {};
+  const { loginForm, history } = action.payload;
+  const { username, password } = loginForm;
+
+  const headers = {
+    "Content-Tpye": "application/json",
+  };
+
+  const body = { username, password };
+
+  try {
+    const res = axios.post(EARTHDAWN_URL + "/v1/login", body, headers);
+    const data = res.data;
+    debugger;
+  } catch (err) {
+    console.error("Error logging in user", { err });
+  }
 }
