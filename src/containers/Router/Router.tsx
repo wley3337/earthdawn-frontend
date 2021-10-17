7f98b495e8b7b88994ea05dbacd9c9b2ac8db1f2
Will Ley <wley3337@gmail.com>
2021-10-17 10:45:33 -0400

    Add first pass autologin

--- src/containers/Router/Router.tsx
+++ src/containers/Router/Router.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { getXCSRFToken } from "../../util/cookie";
+import Landing from "../Landing/Landing";
 import Login from "../Login/Login";
+import Talents from "../Talents/Talents";
 
 export const Router: React.FC = () => {
-  return <Route exact path="/login" component={Login} />;
+  const hasXCSRFToken = !!getXCSRFToken();
+  return (
+    <>
+      {hasXCSRFToken ? (
+        // this should be to check if the token xcsrf token is still valid ( hasn't expired ) before routing
+        <Route exact path="/" component={Landing} />
+      ) : (
+        <Route exact path="/" component={Login} />
+      )}
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/talents" component={Talents} />
+    </>
+  );
 };
