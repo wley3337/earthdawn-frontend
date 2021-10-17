55b9ca41e48fc5d04d703753cd97a751a89004f6
Will Ley <wley3337@gmail.com>
2021-10-17 11:33:31 -0400

    Add prettier config to project

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { Theme } from "@material-ui/core";
-import { createStyles, WithStyles, withStyles } from "@material-ui/styles";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Theme } from '@material-ui/core'
+import { createStyles, WithStyles, withStyles } from '@material-ui/styles'
+import { Link } from 'react-router-dom'
 
 const styles = (theme: Theme) =>
-  createStyles({
-    wrapper: {
-      display: "grid",
-    },
-  });
+    createStyles({
+        wrapper: {
+            display: 'grid',
+        },
+    })
 
 interface Props extends WithStyles<typeof styles> {}
 
 const Navbar: React.FC<Props> = ({ classes }) => {
-  return (
-    <nav className={classes.wrapper}>
-      <Link to="/talents">Talents</Link>
-      <Link to="/character-creation">Character Creation</Link>
-    </nav>
-  );
-};
+    return (
+        <nav className={classes.wrapper}>
+            <Link to="/talents">Talents</Link>
+            <Link to="/character-creation">Character Creation</Link>
+        </nav>
+    )
+}
 
-export default withStyles(styles)(Navbar);
+export default withStyles(styles)(Navbar)
